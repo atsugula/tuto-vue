49bf874d36ef808b3de7933cc98e02bdd1a6e725
-_- <atsgula@gmail.com>
2022-12-23 23:41:00 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+  const name = 'Vue 3';
+  const styleAzul = 'color: blue';
+  const styleAmarillo = 'color: yellow';
+  const arrayColores = ['red','yellow','orange','peru'];
+  const condicional = true;
+</script>
+
+<template>
+  <h1>Hola mundo, {{ name }}</h1>
+  <h2 v-bind:style="styleAzul">color azul</h2>
+  <h2 :style="styleAmarillo">color amarillo</h2>
+  <h2 :style="`color: ${arrayColores[3]}`">color Perú</h2>
+  <h3>{{ condicional ? 'Es verdad' : 'Es falso' }}</h3>
+</template>
+
+<style>
+  h1{
+    color:red
+  }
+</style>
